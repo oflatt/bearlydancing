2c01b8ceb1fb31ba3eff721dddf4e69b5696c5c1
Oliver <oflatt@gmail.com>
2016-03-31 13:33:25 -0600

    change file name
    Test map will be used to look at how big images are relative to each
    other

--- the_new_rpg/src/graphics.py
+++ the_new_rpg/src/graphics.py
@@ -15,7 +15,7 @@ def sscale_customfactor(img, factor):
     return pygame.transform.scale(img, [int((w/smaller)*endsize), int((h/smaller)*endsize)])
 
 def sscale(img):
-    factor = 0.00025 #This basically determines how much of the map we can see
+    factor = 0.0025 #This basically determines how much of the map we can see
     w = img.get_width()
     h = img.get_height()
     endsize = variables.height*factor
@@ -23,6 +23,7 @@ def sscale(img):
         smaller = h
     else:
         smaller = w
+    print(smaller)
     return pygame.transform.scale(img, [int((w/smaller)*endsize*smaller), int((h/smaller)*endsize*smaller)])
 
 #Oliver's example- make sure to put .convert() at the end to make it run faster (as a png)
@@ -33,5 +34,5 @@ test_rock = pygame.image.load(os.path.join('pics', 'pokemon_grass.png')).convert
 test_rock = sscale(test_rock)
 
 #Art by Jacob and Spirit imported
-front_honey = pygame.image.load(os.path.join('pics', 'Honey front SILR.png')).convert()
+front_honey = pygame.image.load(os.path.join('pics', "honeySILR_0.png"))
 front_honey = sscale(front_honey)
