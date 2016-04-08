0e89419f35eff450aa92c7ca3cd973486c8bdc34
Alec Tran <9514555@graniteschools.org>
2016-04-08 13:00:53 -0600

    House Edit

--- the_new_rpg/src/graphics.py
+++ the_new_rpg/src/graphics.py
@@ -151,4 +151,4 @@ tree2 = sscale(pygame.image.load(os.path.join('pics', "treeSILR_0.png")))
 
 tree3 = sscale(pygame.image.load(os.path.join('pics', "SophiaTree_0.png")))
 
-house = sscale(pygame.image.load(os.path.join('pics', "BearHome.png")))
+house = sscale(pygame.image.load(os.path.join('pics', "HouseSILR.png")))
