b27002aaf22a8abc6b197203749236d65239076c
Jacob Valero <9639054@graniteschools.org>
2016-04-12 13:18:28 -0600

    Script

--- the_new_rpg/src/conversations.py
+++ the_new_rpg/src/conversations.py
@@ -4,4 +4,8 @@ from Conversation import Conversation
 
 testconversation = Conversation(["hello", "I am on the second line now", "and by now it should be workin'", "bla", "bla"])
 
-currentconversation = testconversation
+firstscene = Conversation(["Honey: Oh goody its lunch, I'm STARVING! But no, I left my glorious spinach-mushroom-provalone-bacon-broccoli quiche in the forest! I'll have to go get it.",
+                           "Trash Panda: But Honey, those forests are infested with monsters! You shouldn't fight all of them just for a quiche!" ,
+                           "Honey: It is no ordinary quiche, Trash Panda. It is my glorious spinach-mushroom-provalone-bacon-broccoli quiche! Lunch is the most important meal of the day, I need to do this."])
+
+currentconversation = firstscene
