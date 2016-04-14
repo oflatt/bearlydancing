eaaa27887ee1ee1e024f601ebeb028e7c9214280
Alec Tran <9514555@graniteschools.org>
2016-04-14 13:02:08 -0600

    end point start point stuff kek

--- the_new_rpg/src/maps.py
+++ the_new_rpg/src/maps.py
@@ -12,21 +12,25 @@ testmap1.startpoint = [block * 10, block*10]
 testmap1.endarea = [block * 50, block * 50, block * 55, block*55]
 
 #Rock(graphics.(whatever picture), x value, y value, Collision Detection)
-house1 = Map(graphics.houseInside, [Rock(graphics.welcomeMat, 2.25*block, 5.3*block, True),
+house1 = Map(graphics.houseInside, [Rock(graphics.welcomeMat, 2.25*block, 5.3*block, False),
                                    Rock(graphics.bed, 0*block, 0*block, False),
                                    Rock(graphics.warddrobe2, 2*block, 0*block, False),
                                    Rock(graphics.tpanda, 4*block, 5*block, True)])
+#blank.endpoint (top left x, top left y, bottom right x, bottom right y)
+house1.startpoint = [0,0]
+house1.endpoint = [block*2,block*4,block*2.5,block*5.5]
+
 
 houserock = Rock(graphics.house, 0, 0, True)
 houserock.h = houserock.h * 3/5
-outsideiguess1 = Map(graphics.scrub1, [houserock,
+outside1 = Map(graphics.scrub1, [houserock,
                                        Rock(graphics.welcomeMat, 0.4*block,3*block, False),
                                        Rock(graphics.tree1, 5*block, 3*block, True),
                                        Rock(graphics.tree1, 2.1*block, 3*block, True),
                                        Rock(graphics.tree3, 2.2*block, 3*block, True),
                                        Rock(graphics.tree3, 2.5*block, 3.2*block, True)])
 
-outsideiguess1.startpoint = [block *0.85, block*2.9]
-outsideiguess1.endpoint = [block*50,block*50,block*55,block*55]
+outside1.startpoint = [block *0.85, block*2.9]
+outside1.endpoint = [block*50,block*50,block*55,block*55]
 
-current_map = house1
+current_map = outside1
