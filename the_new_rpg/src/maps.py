916036723409dac510f381cc83e17c838618a365
Jacob Valero <9639054@graniteschools.org>
2016-04-26 13:54:29 -0600

    Added more graphics and added more to the maps

--- the_new_rpg/src/maps.py
+++ the_new_rpg/src/maps.py
@@ -19,20 +19,30 @@ housewidth = graphics.house.get_width()
 # outside 1
 outside1 = Map(graphics.scrub1, [houserock,
                                  Rock(graphics.tree3, 1.4*block, 1.9*block, True),
+                                 Rock(graphics.sheep1, 1.1*block, 7.3*block, False),
+                                 Rock(graphics.meanGreen0, 3*block, 2.5*block, False),
+                                 Rock(graphics.purplePerp0, 8*block, 5.3*block, False),
+                                 Rock(graphics.rock, 6.5*block, 7*block, True),
+                                 Rock(graphics.rock, 4.5*block, 3.5*block, False),
                                  Rock(graphics.tree3, 1.9*block, 1.9*block, True),
                                  Rock(graphics.tree3, 2.4*block, 1.9*block, True),
-                                 Rock(graphics.tall_Tree, 4*block, 5*block, True),
-                                 Rock(graphics.tall_Tree, 2.5*block, 5*block, True),
-                                 Rock(graphics.tall_Tree2, 0.5*block, 4*block, True)])
+                                 Rock(graphics.tall_Tree, 3.3*block, 6.7*block, True),
+                                 Rock(graphics.tall_Tree, 4.5*block, 5.3*block, True),
+                                 Rock(graphics.tall_Tree2, 0.5*block, 4.5*block, True),
+                                 Rock(graphics.tall_Tree, 2.9*block, 0.1*block, True),
+                                 Rock(graphics.tall_Tree2, 5.2*block, 8, True),
+                                 Rock(graphics.tall_Tree2, 7.5*block, 8, True),
+                                 Rock(graphics.tall_Tree, 8.2*block, 5.3*block, True),
+                                 Rock(graphics.rock, 2.5*block, 6.3*block, True)])
 
 outsidewidth = graphics.scrub1.get_width()
 outsideheight = graphics.scrub1.get_height()
 outside1.startpoint= [block*8,block*4]
 outside1.exitareas = [Exit([outsidewidth, 0, 100, outsideheight], False, 'outside2', 0, block*4),
                       Exit([housewidth*(1/5), housewidth*(3/5), housewidth*(1/5), housewidth*(3/32)], True, 'honeyhome', 0, 0)]
-#outside1.enemies = [Enemy(graphics.sheep1, 0.9, "sheep"), Enemy(graphics.meanGreen0, 1.0, "greenie")]
-#outside1.lvrange = [1, 2]
-#outside1.conversations = [conversations.testconversation]
+outside1.enemies = [Enemy(graphics.sheep1, 0.9, "sheep"), Enemy(graphics.meanGreen0, 0.7, "greenie"), Enemy(graphics.purplePerp0, 0.5, "purpur")]
+outside1.lvrange = [1, 2]
+outside1.conversations = [conversations.testconversation]
 
 #honeyhome
 insidewidth = graphics.houseInside.get_width()
@@ -44,7 +54,8 @@ honeyhome = Map(graphics.houseInside, [Rock(graphics.welcomeMat,
                                             False),
                                        Rock(graphics.bed, 0*insideb, 0*insideb, False),
                                        Rock(graphics.warddrobe2, 2*insideb, 0*insideb, False),
-                                       Rock(graphics.tpanda, 8*insideb, 7*insideb, False)])
+                                       Rock(graphics.tpanda, 8*insideb, 7*insideb, False),
+                                       Rock(graphics.wiiu, 5*insideb, 0.1*insideb, False)])
 
 honeyhome.startpoint = [0,0]
 honeyhome.exitareas = [Exit([insidewidth/2-graphics.welcomeMat.get_width()/2, insideheight,
@@ -55,12 +66,14 @@ racoonc.isbutton = False
 racoonc.part_of_story = 1 #makes it so you can only have the conversation once
 honeyhome.conversations = [racoonc]
 
-outside2 = Map(graphics.leftTurn, [Rock(graphics.tall_Tree, 4*block, 4*block, True),
-                                   Rock(graphics.tall_Tree2, 6*block, 6*block, True)])
+outside2 = Map(graphics.leftTurn, [Rock(graphics.tall_Tree, 4*block, 5*block, True),
+                                   Rock(graphics.tall_Tree2, 5.5*block, 4.5*block, True),
+                                   Rock(graphics.tall_Tree, 2*block, 5*block, True),
+                                   Rock(graphics.tall_Tree2, 6.7*block, 2*block, True)])
 
 outside2.exitareas = [Exit([outsidewidth, outsideheight, 0, 100], False, 'outside1', 4*block, 4*block)]
 
-current_map = outside1
+current_map = outside2
 classvar.player.teleport(current_map.startpoint[0], current_map.startpoint[1])
 
 def new_scale_offset():
