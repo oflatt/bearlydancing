3545ea977b73045dcc3aff87377f69e2730a4562
Oliver Flatt <oflatt@gmail.com>
2016-08-09 09:41:17 -0600

    Some map works

--- the_new_rpg/src/maps.py
+++ the_new_rpg/src/maps.py
@@ -10,33 +10,26 @@ block = variables.width/10
 
 houserock = Rock(graphics.house, 0, 0, False)
 houserock.collideh = graphics.house["scale-height"] * (3/5)
-#this is not actually accurate because it does not take into account the map scale offset
 housewidth = graphics.house["scale-width"]
+househeight = graphics.house["scale-height"]
+hw = graphics.honey_right0["scale-width"]
+hh = graphics.honey_right0["scale-height"]
 
 # outside 1
 outside1 = Map(graphics.scrub1, [houserock,
                                  Rock(graphics.tree3, 1.4*block, 1.9*block, True),
-                                 Rock(graphics.sheep1, 1.1*block, 7.3*block, False),
-                                 Rock(graphics.meanGreen0, 3*block, 2.5*block, False),
-                                 Rock(graphics.purplePerp0, 8*block, 5.3*block, False),
+                                 Rock(graphics.meanGreen0, 3.2*block, 0.1*block+graphics.tall_Tree["scale-height"]-graphics.meanGreen0["scale-height"], True),
                                  Rock(graphics.rock, 6.5*block, 7*block, True),
-                                 Rock(graphics.rock, 4.5*block, 3.5*block, False),
+                                 Rock(graphics.rock, 4.5*block, 3.5*block, True),
                                  Rock(graphics.tree3, 1.9*block, 1.9*block, True),
                                  Rock(graphics.tree3, 2.4*block, 1.9*block, True),
-                                 Rock(graphics.tall_Tree, 3.3*block, 6.7*block, True),
-                                 Rock(graphics.tall_Tree, 4.5*block, 5.3*block, True),
-                                 Rock(graphics.tall_Tree2, 0.5*block, 4.5*block, True),
-                                 Rock(graphics.tall_Tree, 2.9*block, 0.1*block, True),
-                                 Rock(graphics.tall_Tree2, 5.2*block, 8, True),
-                                 Rock(graphics.tall_Tree2, 7.8*block, 8, True),
-                                 Rock(graphics.tall_Tree, 7.9*block, 5.3*block, True),
                                  Rock(graphics.rock, 2.5*block, 6.3*block, True)])
 
 outsidewidth = graphics.scrub1["scale-width"]
 outsideheight = graphics.scrub1["scale-height"]
 outside1.startpoint= [block*8,block*4]
 outside1.exitareas = [Exit([outsidewidth, 0, 100, outsideheight], False, 'outside2', 25, outsideheight/2),
-                      Exit([housewidth*(1/5), housewidth*(3/5), housewidth*(1/5), housewidth*(3/32)], True, 'honeyhome', block*4.3, block*8 )]
+                      Exit([housewidth*(1/5), househeight*(3/5), housewidth*(1.5/10), househeight*(2/5)], True, 'honeyhome', block*5-(hw/2), block*10-(hh))]
 outside1.enemies = [Enemy(graphics.sheep1, 0.5, "sheep"), Enemy(graphics.meanGreen0, 0.3, "greenie"), Enemy(graphics.purplePerp0, 0.2, "purpur")]
 outside1.lvrange = [1,1]
 outside1c = conversations.secondscene
@@ -47,6 +40,7 @@ greenie = Enemy(graphics.meanGreen0,0.3, "Greenie Meanie")
 greenie.lv = 1
 outside1c.special_battle = Enemy(graphics.meanGreen0, 1, "Greenie Meanie")
 outside1.conversations = [outside1c]
+outside1.foreground_terrain = [Rock(graphics.tall_Tree, 2.9*block, 0.1*block, False)]
 
 
 #honeyhome
@@ -65,7 +59,7 @@ honeyhome.startpoint = [0,0]
 honeyhome.exitareas = [Exit([insidewidth/2-graphics.welcomeMat["scale-width"]/2, insideheight,
                              graphics.welcomeMat["scale-width"], insideb], False, 'outside1', insideb*0.9, insideb*4)]
 racoonc = conversations.firstscene
-racoonc.area = [0, 7*insideb, insidewidth, 50]#50 because it does not matter how thick it is down
+racoonc.area = [0, 7*insideb+graphics.tpanda["scale-height"], insidewidth, 50]#50 because it does not matter how thick it is down
 racoonc.isbutton = False
 racoonc.part_of_story = 1 #makes it so you can only have the conversation once
 honeyhome.conversations = [racoonc]
