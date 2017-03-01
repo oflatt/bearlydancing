252df5af15e11135af1694c60d905d5a54c0a4be
Oliver Flatt <oflatt@gmail.com>
2017-02-28 18:25:53 -0700

    begin animation system for rocks, make stash better
    Now the trees look small because the house is big

--- the_new_rpg/src/Rock.py
+++ the_new_rpg/src/Rock.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 import pygame, variables
 
+
 class Rock():
     collidex = None
     collidey = None
     collidew = None
     collideh = None
-    #background range is the range of the player's location that it is drawn behind the player
-    background_range = pygame.Rect(0,0,variables.width*100,variables.height*100)
+    # background range is the range of the player's location that it is drawn behind the player
+    background_range = pygame.Rect(0, 0, variables.width * 100, variables.height * 100)
+    animation = None
 
     def __init__(self, base, x, y, collidesection):
-        #collidesection is a list x y width height all of the arguments are relative to the rock's pos and dimensions
-        #width and height of collidesection are multiplied by the width and height of the base
+        # collidesection is a list x y width height all of the arguments are relative to the rock's pos and dimensions
+        # width and height of collidesection are multiplied by the width and height of the base
         self.collidesection = collidesection
         if self.collidesection == None:
             self.collidesection = [0, 0, 0, 0]
@@ -23,24 +25,27 @@ class Rock():
         self.make_mask(True)
 
     def draw(self):
-        variables.screen.blit(self.base["img"], [self.x, self.y])
+        if not self.animation == None:
+            variables.screen.blit(self.animation.current_frame())
+        else:
+            variables.screen.blit(self.base["img"], [self.x, self.y])
 
     def make_mask(self, isresetbackgroundrange):
         cs = self.collidesection
         maskpic = self.base["img"].copy()
         w = self.base["img"].get_width()
         h = self.base["img"].get_height()
-        #fill all but the collide section
-        maskpic.fill(pygame.Color(0, 0, 0, 0), [0, 0, w, cs[1]*h])
-        maskpic.fill(pygame.Color(0, 0, 0, 0), [0, 0, cs[0]*w, h])
-        maskpic.fill(pygame.Color(0, 0, 0, 0), [cs[0]*w + cs[2]*w, 0, w-(cs[0]*w + cs[2]*w), h])
-        maskpic.fill(pygame.Color(0, 0, 0, 0), [0, cs[1]*h+cs[3]*h, w, h-(cs[1]*h+cs[3]*h)])
+        # fill all but the collide section
+        maskpic.fill(pygame.Color(0, 0, 0, 0), [0, 0, w, cs[1] * h])
+        maskpic.fill(pygame.Color(0, 0, 0, 0), [0, 0, cs[0] * w, h])
+        maskpic.fill(pygame.Color(0, 0, 0, 0), [cs[0] * w + cs[2] * w, 0, w - (cs[0] * w + cs[2] * w), h])
+        maskpic.fill(pygame.Color(0, 0, 0, 0), [0, cs[1] * h + cs[3] * h, w, h - (cs[1] * h + cs[3] * h)])
         self.mask = pygame.mask.from_surface(maskpic)
 
         h = self.base["h"]
         # by default background range is by the top of the mask, the collision box
-        if(not cs == [0,0,1,1] and isresetbackgroundrange):
-            self.background_range = pygame.Rect(0, self.y + cs[1] * h + cs[3]*(1/3)*h,
+        if (not cs == [0, 0, 1, 1] and isresetbackgroundrange):
+            self.background_range = pygame.Rect(0, self.y + cs[1] * h + cs[3] * (1 / 3) * h,
                                                 variables.width * 100,
                                                 variables.height * 100)
 
@@ -48,11 +53,15 @@ class Rock():
         s = scale
         self.x *= s
         self.y *= s
-        #scale base pic to right size
-        self.base["img"] = pygame.transform.scale(self.base["img"], [int(self.base["w"]*s),
-                                                                     int(self.base["h"]*s)])
+        # scale base pic to right size
+        self.base["img"] = pygame.transform.scale(self.base["img"], [int(self.base["w"] * s),
+                                                                     int(self.base["h"] * s)])
+        for x in range(len(self.animation.pics)):
+            self.animation.pics[x]["img"] = pygame.transform.scale(self.animation.pics[x]["img"],
+                                                                   [int(self.base["w"] * s), int(self.base["h"] * s)])
         self.w = self.base["img"].get_width()
         self.h = self.base["img"].get_height()
+
         if self.collidex == None:
             self.collidex = self.x
         else:
@@ -72,9 +81,11 @@ class Rock():
         else:
             self.collideh *= s
         if self.background_range == None:
-            self.background_range = pygame.Rect(-1000, -1000, 0,0)
+            self.background_range = pygame.Rect(-1000, -1000, 0, 0)
+
         self.background_range.x *= s
         self.background_range.y *= s
         self.background_range.width *= s
         self.background_range.height *= s
+
         self.make_mask(False)
