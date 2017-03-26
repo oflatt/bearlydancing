7b4027aa75922bff3ba829ae6d02620b05a48bfa
Oliver Flatt <oflatt@gmail.com>
2017-03-26 09:15:52 -0600

    Finish random trees- next put them in game

--- rdraw.py
+++ rdraw.py
@@ -1,13 +1,19 @@
-import pygame, variables, copy
+import pygame, variables, copy, random
 from pygame import draw
 from random import randint
 
 TREEWIDTH = 100
 TREEHEIGHT = 200
+TREEFILLCOLOR = (33, 52, 27, 255)
+TREEOUTLINECOLOR = (22, 40, 20)
+TRUNKCOLOR = (39, 32, 32, 255)
 
 
-def addpoints(l, leftbound, rightbound):
-    maxvariation = 4
+def pointinbounds(p):
+    return p[0] >= 0 and p[0] < TREEWIDTH and p[1] >= 0 and p[1] < TREEHEIGHT
+
+
+def addpoints(l, leftbound, rightbound, maxvariation):
     cl = copy.deepcopy(l)
     length = len(l)
     x = 0
@@ -40,8 +46,6 @@ def drawlayer(p, yoffset, leftbound, rightbound, istoplayer=False):
     middlebound = halflen + leftbound
     spikelower = 108 + yoffset
     spikeupper = 92 + yoffset
-    outlinecolor = [22, 40, 20]
-    fillcolor = [33, 52, 27]
     spikewmin = 4
     spikewmax = 8
     # highest point unless istoplayer is true
@@ -65,7 +69,7 @@ def drawlayer(p, yoffset, leftbound, rightbound, istoplayer=False):
 
     fourth = int(boundwidth / 4)
     if istoplayer:
-        #with the top layer, it's taller and has points in the middle
+        # with the top layer, it's taller and has points in the middle
         rightpoints.extend(
             [[rightbound - fourth, 70 + yoffset],
              [middlebound + randint(0, fourth), randint(52, 65) + yoffset],
@@ -92,12 +96,11 @@ def drawlayer(p, yoffset, leftbound, rightbound, istoplayer=False):
 
     rightpoints.extend(leftpoints[::-1])
     # Then call a function that randomly adds new points for complexity
-    rightpoints = addpoints(rightpoints, leftbound, rightbound)
-    print(rightpoints)
+    rightpoints = addpoints(rightpoints, leftbound, rightbound, 4)
 
-    startingpoint = [middlebound, int((100+yoffset - topy) / 2) + topy]
+    startingpoint = [middlebound, int((100 + yoffset - topy) / 2) + topy]
     insidecolorbefore = p.get_at(startingpoint)
-    draw.polygon(p, outlinecolor, rightpoints, 1)  # outline
+    draw.polygon(p, TREEOUTLINECOLOR, rightpoints, 1)  # outline
 
     # fills a polygon with a point in the polygon
     def fillpolygon(s, firstpoint):
@@ -105,7 +108,7 @@ def drawlayer(p, yoffset, leftbound, rightbound, istoplayer=False):
 
         while len(pointlist) != 0:
             point = pointlist.pop(0)
-            s.set_at(point, fillcolor)
+            s.set_at(point, TREEFILLCOLOR)
             if (point[0] > 0):
                 if (s.get_at([point[0] - 1, point[1]]) == insidecolorbefore):
                     pointlist.insert(0, [point[0] - 1, point[1]])
@@ -121,8 +124,114 @@ def drawlayer(p, yoffset, leftbound, rightbound, istoplayer=False):
 
     fillpolygon(p, startingpoint)
 
+
 def drawtrunk(surface):
-    surface.fill([39, 32, 32, 255], [int(TREEWIDTH/2)-7, int(TREEHEIGHT/2), 14, int(TREEHEIGHT/2)])
+    midpoint = int(TREEWIDTH / 2)
+    halfh = int(TREEHEIGHT / 2)
+    bottom = TREEHEIGHT - 1
+
+    pl = [[0, 0],
+          [3, -1],
+          [7, -2],
+          [10, -4],
+          [12, -8],
+          [10, -12],
+          [7, -15],
+          [7, -halfh]]
+
+    reversepl = copy.deepcopy(pl)
+    reversepl = reversepl[::-1]
+    for x in reversepl:
+        x[0] = -x[0]
+    pl.extend(reversepl)
+
+    # center it at the right place
+    for x in pl:
+        x[0] += midpoint
+        x[1] += bottom
+
+    def changeamount(p, changedir):
+        # this is so that if it changes towards the center, it changes much less (prevents trees with empty regions
+        if p[0] > midpoint:
+            if changedir < 0:
+                a = randint(1, 3) * changedir
+            else:
+                a = randint(3, 10) * changedir
+        else:
+            if changedir < 0:
+                a = randint(3, 10) * changedir
+            else:
+                a = randint(1, 3) * changedir
+        return a
+
+    pl = addpoints(pl, 0, TREEWIDTH, 3)
+    # variation- x positions first
+    for x in range(len(pl)):
+        if randint(1, 5) == 1:
+            changedir = random.choice([-1, 1])
+            pl[x][0] += changeamount(pl[x], changedir)
+            a = 1
+            while randint(1, 3) > 1:
+                pl[(x + a) % len(pl)][0] += changeamount(pl[(x + a) % len(pl)], changedir)
+                a += 1
+
+    draw.polygon(surface, TRUNKCOLOR, pl)
+
+
+#specialtextures is a list of colors to use instead of the normal ones
+def texturepoint(surface, x, y, searchcolor, type, specialtextures):
+    moss1 = (28, 61, 33)
+    moss2 = (26, 47, 21)
+    moss3 = (28, 63, 12)
+    bark1 = (45, 32, 32)
+    bark2 = (46, 35, 35)
+
+    def patchoftexturechance(t, defaultchance, xcfactor, ycfactor):
+        points = [[x, y]]
+        while len(points) > 0:
+            p = points.pop(0)
+            if pointinbounds(p):
+                if surface.get_at(p) == searchcolor:
+                    chance = defaultchance
+                    if p[0] != x:
+                        chance *= xcfactor
+                    if p[1] != y:
+                        chance *= ycfactor
+
+                    if random.random() <= 1/chance:
+                        surface.set_at(p, t)
+                        # then go again on the points to the side and the one below
+                        points.extend([[p[0], p[1] + 1], [p[0] - 1, p[1]], [p[0] + 1, p[1]]])
+
+    if surface.get_at([x, y]) == searchcolor:
+        if type == "moss":
+            if randint(1, 80) == 1:
+                surface.set_at([x, y], moss1)
+            elif randint(1, 10) == 1:
+                patchoftexturechance(moss3, 7, 3, 1/4)
+            else:
+                if specialtextures != None:
+                    patchoftexturechance(specialtextures[0], 7, 3, 1 / 4)
+                else:
+                    patchoftexturechance(moss2, 7, 3, 1/4)
+        if type == "bark":
+            if randint(1, 5) == 1:
+                patchoftexturechance(bark1, 20, 8, 1/18)
+            elif randint(1, 2) == 1:
+                if specialtextures != None:
+                    patchoftexturechance(specialtextures[0], 20, 8, 1/18)
+                else:
+                    patchoftexturechance(bark2, 20, 8, 1 / 18)
+
+def texturerow(surface, y, searchcolor, type, specialtextures):
+    for x in range(surface.get_width()):
+        texturepoint(surface, x, y, searchcolor, type, specialtextures)
+
+#type is a string that refers to a set of textures and random patterns
+def addtexture(surface, searchcolor, type, specialtextures = None):
+    for y in range(surface.get_height()):
+        texturerow(surface, y, searchcolor, type, specialtextures)
+
 
 def drawtree(surface):
     p = pygame.Surface([TREEWIDTH, TREEHEIGHT], pygame.SRCALPHA)
@@ -147,10 +256,26 @@ def drawtree(surface):
     yoffset += randint(-25, -15)
     drawlayer(l4, yoffset, 15, TREEWIDTH - 15, True)
 
-    p.blit(l, [0,0])
+    p.blit(l, [0, 0])
     p.blit(l2, [0, 0])
     p.blit(l3, [0, 0])
     p.blit(l4, [0, 0])
+
+    #chance for special texture
+    if randint(1, 3) == 1:
+        addtexture(p, TREEFILLCOLOR, "moss", [(randint(22, 28), randint(45, 65), randint(10, 25))])
+    else:
+        addtexture(p, TREEFILLCOLOR, "moss")
+
+    #chance to add a different special texture
+    if randint(1, 10) == 1:
+        addtexture(p, TRUNKCOLOR, "bark", [(randint(30, 45), randint(30, 45), randint(30, 45))])
+    else:
+        addtexture(p, TRUNKCOLOR, "bark")
+
+    # chance to add moss to tree
+    if randint(1, 9) == 1:
+        addtexture(p, TRUNKCOLOR, "moss", [(randint(22, 28), randint(45, 65), randint(10, 25))])
 
     p = pygame.transform.scale(p, [TREEWIDTH * 4, TREEHEIGHT * 4])
     surface.blit(p, [0, 0])
