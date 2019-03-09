9ba77c8986190c84eb4af39630e59eed1e0705e9
oliver <oflatt@gmail.com>
2019-03-09 04:32:54 -0700

    spike on rose

--- src/growgame/drawplant.py
+++ src/growgame/drawplant.py
@@ -20,8 +20,8 @@ def shiftpointlist(pointlistin, shiftchance, widthscalar, heightscalar):
             else:
                 shiftamount -= 1
 
-        l[i] = (l[i][0]*widthscalar, l[i][1]*heightscalar+shiftamount)
-        l[-i-1] = (l[-i-1][0]*widthscalar, l[-i-1][1]*heightscalar+shiftamount)
+        l[i] = (l[i][0]*heightscalar, l[i][1]*widthscalar+shiftamount)
+        l[-i-1] = (l[-i-1][0]*heightscalar, l[-i-1][1]*widthscalar+shiftamount)
 
     return l
 
@@ -46,9 +46,55 @@ def transformtopointlists(plantshapelist, shiftchance, angle, offset, widthscala
     
     return listofpolygonlists
 
+
+
+# returns a list of index positions in a polygon list on which to spawn new nodes
+# takes the number of nodes to put on, the length of the parent node polygon list
+# and the percent of the polygon list that is available for spawning on
+def getfuturenodeindexpositions(numberofnodes, polygonlistlength, percentofbranch):
+    
+    if percentofbranch > 1:
+        raise Exception("percent of branch must be 1 or less and 0 or more")
+    # first make dispersed distruibution for the new nodes
+    dispersedpositions = []
+    total = 0
+    for x in range(numberofnodes+1):
+        # half of the spacing is random
+        dispersedpositions.append((random.random()/numberofnodes)/2 + (1/numberofnodes)/2)
+        total += dispersedpositions[-1]
+
+    # normalize it to fit into 1
+    normalize_factor = 1.0/total
+    normalize_factor = normalize_factor * (polygonlistlength/2) * percentofbranch
+
+    # offset
+    offset = (polygonlistlength/2) - (polygonlistlength/2)*percentofbranch
+    
+    # omit the last space because it is now not needed
+    dispersedpositions.pop()
+
+    indexes = []
+    currentpos = offset
+    
+    # now translate it into the space of the part of the polygonlist
+    for i in range(len(dispersedpositions)):
+        currentpos += dispersedpositions[i] * normalize_factor
+        # half chance to be on the other side
+        if random.random() < 0.5:
+            indexes.append(int(polygonlistlength-currentpos))
+        else:
+            indexes.append(int(currentpos))
+
+    # should be the same length as number of nodes
+    if not numberofnodes == len(indexes):
+        raise Exception("Bad number of indexes")
+    return indexes
+
+    
+
+
 def drawplant(head_node):
     surface = Surface((40, 40), SRCALPHA)
-    
     
     
     # the stack has the node, the currentx, and the currenty for each node in it
@@ -75,10 +121,10 @@ def drawplant(head_node):
             randomspacings.append(random.uniform(node.anglespace-node.anglevariance*node.anglespace, node.anglespace+node.anglevariance*node.anglespace))
             spacingLength += randomspacings[i+1]
 
-        startspacing = random.uniform(node.anglespace-node.anglevariance*node.anglespace, node.anglespace+node.anglevariance*node.anglespace)/2 * random.choice((-1, 1))
+        startspacing = random.uniform(-node.anglevariance*node.anglespace, node.anglevariance*node.anglespace)/2 * random.choice((-1, 1))
         
         # start angle so that it points up on average
-        angle = -math.pi/2 + startspacing - spacingLength/2 + node.angleoffset
+        angle = -math.pi/2 + startspacing - (spacingLength/2)  + node.angleoffset*random.choice((-1, 1))
 
         # update the random spacing to be final angles
         for i in range(len(randomspacings)):
@@ -105,18 +151,16 @@ def drawplant(head_node):
 
             
             # find the new currentx and currenty
-            mainlistlen = len(node.plantshapelist[0].polygonlist)
             mainltranslated = pointliststransformed[0]
+            mainlistlen = len(mainltranslated)
 
             
             # add all the children at the current position
             for childnode in node.children:
+                futureindexpositions = getfuturenodeindexpositions(childnode.repeatnumseparate, mainlistlen, childnode.brancharea)
                 for i in range(childnode.repeatnumseparate):
-                    
-                    randindex = random.randint(int(mainlistlen/2 - mainlistlen*childnode.brancharea),
-                                               int(mainlistlen/2 + mainlistlen*childnode.brancharea))
-                    futurex = mainltranslated[randindex][0]
-                    futurey = mainltranslated[randindex][1]
+                    futurex = mainltranslated[futureindexpositions[i]][0]
+                    futurey = mainltranslated[futureindexpositions[i]][1]
                     stack.append(childnode)
                     stack.append(futurex)
                     stack.append(futurey)
--- src/growgame/shopplants.py
+++ src/growgame/shopplants.py
@@ -1,7 +1,7 @@
 import math
 
 from DestructiveFrozenClass import DestructiveFrozenClass
-
+from variables import brighten
 
 from .PlantNode import PlantNode
 from .PlantShape import PlantShape
@@ -53,17 +53,37 @@ def add_rose():
     petal_numofpoints = 13
     
     for x in range(petal_numofpoints):
-        petal_list.append((x, 4*math.sin((1-(x**1.5/petal_numofpoints**1.5)) * math.pi)))
+        petal_list.append((x*0.75, 2*math.sin((1-(x**1.5/petal_numofpoints**1.5)) * math.pi)))
 
     petal_shape = PlantShape(petal_list, (120, 0, 0), (180, 0, 0))
-    petalnode = PlantNode([petal_shape], 3, (0, 0), math.pi/20)
-    petalnode = petalnode.destructiveset("anglevariance", 3)
-    petalnode = petalnode.destructiveset("heightvariance", 0.75)
+    petalnode = PlantNode([petal_shape], 4, (0, 0), math.pi/8)
+    petalnode = petalnode.destructiveset("anglevariance", 0.3)
+    petalnode = petalnode.destructiveset("heightvariance", 0.2)
+    petalnode = petalnode.destructiveset("widthvariance", 1)
+
     
-    rose = PlantNode([bigstem_plantshape], 1, (0, 0), math.pi/10, children = [petalnode])
+    innerpetal_list = [(0, 0)]
+    innerpetal_numofpoints = 13
+    
+    for x in range(innerpetal_numofpoints):
+        innerpetal_list.append((x*0.75, 1.5*math.sin((1-(x**1.5/innerpetal_numofpoints**1.5)) * math.pi)))
+
+    innerpetal_shape = PlantShape(innerpetal_list, (100, 0, 0), (120, 0, 0))
+    innerpetalnode = PlantNode([innerpetal_shape], 4, (0, 0), 0)
+    innerpetalnode = innerpetalnode.destructiveset("anglevariance", 0)
+    innerpetalnode = innerpetalnode.destructiveset("heightvariance", 0.4)
+
+    spikes_list = [(0, 0.5), (2, 0)]
+    spikes_shape = PlantShape(spikes_list, (50, 70, 50), brighten((50, 85, 50), 20))
+    spikes_node = PlantNode([spikes_shape], 1, (0, 0), math.pi)
+    spikes_node = spikes_node.destructiveset("angleoffset", math.pi/2)
+    spikes_node = spikes_node.destructiveset("repeatnumseparate", 4)
+    spikes_node = spikes_node.destructiveset("brancharea", 1)
+    
+    rose = PlantNode([bigstem_plantshape], 1, (0, 0), math.pi/10, children = [petalnode, innerpetalnode, spikes_node])
 
     addshopplant(ShopPlant("rose", rose, 20))
-        
+
 
 add_starter()
 add_rose()
